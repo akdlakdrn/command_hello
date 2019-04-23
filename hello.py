e8790fc4962ed6dbcd4f8ddb77d0770ec923e11e
hojung kang <akdlakdrn@naver.com>
2019-04-23 10:35:09 +0900

    hello.py modified on local repository

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-// For command line git tutorial
-// First: GitHub modification
-// Second: Local repository modification
+//For command line git tutorial
+//local repository modification
 
 print("Hello world")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
 print("Tell my world")
+print("Git Hell")
